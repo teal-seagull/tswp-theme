6da94f318f305c07741aa9fd11fdcf0401295147
Rafael J. Rodriguez <rafase282@gmail.com>
2014-08-14 13:15:55 -0400

    Updating Files
    Added files from the server to the git, some were missing

--- content.php
+++ content.php
@@ -1,17 +1,93 @@
-<?php
-/**
- * The default template for displaying content
- *
- * Used for both single and index/archive/search.
- *
- * @package WordPress
- * @subpackage tealseagull
- */
-?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	Title: <?php the_title(); ?>
-	Content:
-	the_excerpt: <?php the_excerpt(); ?>
-	Continue: <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'tealseagull' ) ); ?>
-	Author: <?php the_author(); ?>
-</article><!-- #post -->
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage tealseagull
+ */
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<div class="entry-meta entry-time">
+			<span class="entry-time-day"><?php the_time('j') ?></span>
+			<span class="entry-time-month"><?php the_time('M') ?></span>
+			<span class="entry-format-icon"></span>
+		</div>
+		<h2 class="entry-title">
+			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'tealseagull' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+				<?php the_title(); ?>
+			</a>
+		</h2>		
+	</header><!-- .entry-header -->
+
+	<?php if ( is_home() || is_search() || is_archive() ) : // Only display Excerpts for Homepage and Search ?>
+
+		<div class="entry-summary">
+			
+			<?php if ( get_post_format() ) : ?>
+
+				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'tealseagull' ) ); ?>
+						
+  			<?php else : ?>
+  			
+  				<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+					<div class="post-thumbnail">
+						<?php the_post_thumbnail(); ?>
+					</div>
+				<?php endif; ?>
+				<?php tealseagull_excerpt(40); ?>
+				
+			<?php endif; // End if get_post_format() ?>
+
+		</div><!-- .entry-summary -->
+
+	<?php else : ?>
+		<div class="entry-content">
+			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'tealseagull' ) ); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'tealseagull' ), 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+	<?php endif; ?>
+
+	<footer class="entry-meta">
+		
+		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$categories_list = get_the_category_list( __( ', ', 'tealseagull' ) );
+				 if ( $categories_list && tealseagull_categorized_blog() ) :
+			?>
+			<span class="cat-links">
+				<span class="icon-font icon-category-alt"></span>
+				<?php printf( __( '%1$s', 'tealseagull' ), $categories_list ); ?>
+			</span>
+			<?php  endif; // End if categories ?>
+
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$tags_list = get_the_tag_list( '', __( ', ', 'tealseagull' ) );
+				if ( $tags_list ) :
+			?>
+			<span class="sep"></span>
+			<span class="tags-links">
+				<span class="icon-font icon-tag-alt"></span>
+				<?php printf( __( '%1$s', 'tealseagull' ), $tags_list ); ?>
+			</span>
+			<?php endif; // End if $tags_list ?>
+		<?php endif; // End if 'post' == get_post_type() ?>
+
+		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+		<span class="sep"></span>
+		<span class="comments-link">
+			<?php comments_popup_link( __( '<span class="icon-font icon-comment-alt"></span> Leave a comment', 'tealseagull' ), __( '<span class="icon-font icon-comment"></span> 1 Comment', 'tealseagull' ), __( '<span class="icon-font icon-comments-alt"></span> % Comments', 'tealseagull' ) ); ?>
+		</span>
+		<?php endif; ?>
+
+		<?php edit_post_link( __( 'Edit', 'tealseagull' ), '<span class="sep"></span><span class="edit-link">', '</span>' ); ?>
+		
+	</footer><!-- .entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
+
+
+
--- functions.php
+++ functions.php
@@ -1,169 +1,169 @@
-<?php
-/**
- * TealSeagull functions and definitions
- *
- * Sets up the theme and provides some helper functions, which are used
- * in the theme as custom template tags. Others are attached to action and
- * filter hooks in WordPress to change core functionality.
- *
- * When using a child theme (see http://codex.wordpress.org/Theme_Development and
- * http://codex.wordpress.org/Child_Themes), you can override certain functions
- * (those wrapped in a function_exists() call) by defining them first in your child theme's
- * functions.php file. The child theme's functions.php file is included before the parent
- * theme's file, so the child theme functions would be used.
- *
- * Functions that are not pluggable (not wrapped in function_exists()) are instead attached
- * to a filter or action hook.
- *
- */
-
-function tealseagull_boostrap_styles()
-{
-	wp_enqueue_style( 'boostrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, '1.0', 'all' );
-	wp_enqueue_style( 'carousel', get_template_directory_uri() . '/assets/css/carousel.css', false, '1.0', 'all' );
-}
-
-add_action( 'wp_enqueue_scripts', 'tealseagull_boostrap_styles' );
-
-// Make theme available for translation
-	// Translations can be filed in the /languages/ directory
-	load_theme_textdomain( 'tealseagull', TEMPLATEPATH . '/languages' );
-	
-	add_theme_support( 'menus' );
-
-	$locale = get_locale();
-	$locale_file = TEMPLATEPATH . "/languages/$locale.php";
-	if ( is_readable($locale_file) )
-	    require_once($locale_file);
-
-	// Get the page number
-	function get_page_number() {
-	    if ( get_query_var('paged') ) {
-	        print ' | ' . __( 'Page ' , 'tealseagull') . get_query_var('paged');
-	    }
-	} // end get_page_number
-
-	// Custom callback to list comments in the tealseagull style
-	function custom_comments($comment, $args, $depth) {
-	  $GLOBALS['comment'] = $comment;
-	    $GLOBALS['comment_depth'] = $depth;
-	  ?>
-	    <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-	        <div class="comment-author vcard"><?php commenter_link() ?></div>
-	        <div class="comment-meta"><?php printf(__('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment">Permalink</a>', 'tealseagull'),
-	                    get_comment_date(),
-	                    get_comment_time(),
-	                    '#comment-' . get_comment_ID() );
-	                    edit_comment_link(__('Edit', 'tealseagull'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
-	  <?php if ($comment->comment_approved == '0') _e("\t\t\t\t\t<span class='unapproved'>Your comment is awaiting moderation.</span>\n", 'tealseagull') ?>
-	          <div class="comment-content">
-	            <?php comment_text() ?>
-	        </div>
-	        <?php // echo the comment reply link
-	            if($args['type'] == 'all' || get_comment_type() == 'comment') :
-	                comment_reply_link(array_merge($args, array(
-	                    'reply_text' => __('Reply','tealseagull'),
-	                    'login_text' => __('Log in to reply.','tealseagull'),
-	                    'depth' => $depth,
-	                    'before' => '<div class="comment-reply-link">',
-	                    'after' => '</div>'
-	                )));
-	            endif;
-	        ?>
-	<?php } // end custom_comments
-	
-	// Custom callback to list pings
-	function custom_pings($comment, $args, $depth) {
-	       $GLOBALS['comment'] = $comment;
-	        ?>
-	            <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
-	                <div class="comment-author"><?php printf(__('By %1$s on %2$s at %3$s', 'tealseagull'),
-	                        get_comment_author_link(),
-	                        get_comment_date(),
-	                        get_comment_time() );
-	                        edit_comment_link(__('Edit', 'tealseagull'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
-	    <?php if ($comment->comment_approved == '0') _e('\t\t\t\t\t<span class="unapproved">Your trackback is awaiting moderation.</span>\n', 'tealseagull') ?>
-	            <div class="comment-content">
-	                <?php comment_text() ?>
-	            </div>
-	<?php } // end custom_pings
-	
-	
-	// For category lists on category archives: Returns other categories except the current one (redundant)
-	function cats_meow($glue) {
-	    $current_cat = single_cat_title( '', false );
-	    $separator = "\n";
-	    $cats = explode( $separator, get_the_category_list($separator) );
-	    foreach ( $cats as $i => $str ) {
-	        if ( strstr( $str, ">$current_cat<" ) ) {
-	            unset($cats[$i]);
-	            break;
-	        }
-	    }
-	    if ( empty($cats) )
-	        return false;
-
-	    return trim(join( $glue, $cats ));
-	} // end cats_meow
-	
-	// For tag lists on tag archives: Returns other tags except the current one (redundant)
-	function tag_ur_it($glue) {
-	    $current_tag = single_tag_title( '', '',  false );
-	    $separator = "\n";
-	    $tags = explode( $separator, get_the_tag_list( "", "$separator", "" ) );
-	    foreach ( $tags as $i => $str ) {
-	        if ( strstr( $str, ">$current_tag<" ) ) {
-	            unset($tags[$i]);
-	            break;
-	        }
-	    }
-	    if ( empty($tags) )
-	        return false;
-
-	    return trim(join( $glue, $tags ));
-	} // end tag_ur_it
-	
-	// Register widgetized areas
-	function theme_widgets_init() {
-	    // Area 1
-	    register_sidebar( array (
-	    'name' => 'Primary Widget Area',
-	    'id' => 'primary_widget_area',
-	    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-	    'after_widget' => "</li>",
-	    'before_title' => '<h3 class="widget-title">',
-	    'after_title' => '</h3>',
-	  ) );
-
-	    // Area 2
-	    register_sidebar( array (
-	    'name' => 'Secondary Widget Area',
-	    'id' => 'secondary_widget_area',
-	    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-	    'after_widget' => "</li>",
-	    'before_title' => '<h3 class="widget-title">',
-	    'after_title' => '</h3>',
-	  ) );
-	} // end theme_widgets_init
-
-	add_action( 'init', 'theme_widgets_init' );
-	
-	$preset_widgets = array (
-	    'primary_widget_area'  => array( 'search', 'pages', 'categories', 'archives' ),
-	    'secondary_widget_area'  => array( 'links', 'meta' )
-	);
-	if ( isset( $_GET['activated'] ) ) {
-	    update_option( 'sidebars_widgets', $preset_widgets );
-	}
-	// update_option( 'sidebars_widgets', NULL );
-	
-	// Check for static widgets in widget-ready areas
-	function is_sidebar_active( $index ){
-	  global $wp_registered_sidebars;
-
-	  $widgetcolums = wp_get_sidebars_widgets();
-
-	  if ($widgetcolums[$index]) return true;
-
-	    return false;
+<?php
+/**
+ * TealSeagull functions and definitions
+ *
+ * Sets up the theme and provides some helper functions, which are used
+ * in the theme as custom template tags. Others are attached to action and
+ * filter hooks in WordPress to change core functionality.
+ *
+ * When using a child theme (see http://codex.wordpress.org/Theme_Development and
+ * http://codex.wordpress.org/Child_Themes), you can override certain functions
+ * (those wrapped in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before the parent
+ * theme's file, so the child theme functions would be used.
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are instead attached
+ * to a filter or action hook.
+ *
+ */
+
+function tealseagull_boostrap_styles()
+{
+	wp_enqueue_style( 'boostrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, '1.0', 'all' );
+	wp_enqueue_style( 'carousel', get_template_directory_uri() . '/assets/css/carousel.css', false, '1.0', 'all' );
+}
+
+add_action( 'wp_enqueue_scripts', 'tealseagull_boostrap_styles' );
+
+// Make theme available for translation
+	// Translations can be filed in the /languages/ directory
+	load_theme_textdomain( 'tealseagull', TEMPLATEPATH . '/languages' );
+	
+	add_theme_support( 'menus' );
+
+	$locale = get_locale();
+	$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+	if ( is_readable($locale_file) )
+	    require_once($locale_file);
+
+	// Get the page number
+	function get_page_number() {
+	    if ( get_query_var('paged') ) {
+	        print ' | ' . __( 'Page ' , 'tealseagull') . get_query_var('paged');
+	    }
+	} // end get_page_number
+
+	// Custom callback to list comments in the tealseagull style
+	function custom_comments($comment, $args, $depth) {
+	  $GLOBALS['comment'] = $comment;
+	    $GLOBALS['comment_depth'] = $depth;
+	  ?>
+	    <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
+	        <div class="comment-author vcard"><?php commenter_link() ?></div>
+	        <div class="comment-meta"><?php printf(__('Posted %1$s at %2$s <span class="meta-sep">|</span> <a href="%3$s" title="Permalink to this comment">Permalink</a>', 'tealseagull'),
+	                    get_comment_date(),
+	                    get_comment_time(),
+	                    '#comment-' . get_comment_ID() );
+	                    edit_comment_link(__('Edit', 'tealseagull'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
+	  <?php if ($comment->comment_approved == '0') _e("\t\t\t\t\t<span class='unapproved'>Your comment is awaiting moderation.</span>\n", 'tealseagull') ?>
+	          <div class="comment-content">
+	            <?php comment_text() ?>
+	        </div>
+	        <?php // echo the comment reply link
+	            if($args['type'] == 'all' || get_comment_type() == 'comment') :
+	                comment_reply_link(array_merge($args, array(
+	                    'reply_text' => __('Reply','tealseagull'),
+	                    'login_text' => __('Log in to reply.','tealseagull'),
+	                    'depth' => $depth,
+	                    'before' => '<div class="comment-reply-link">',
+	                    'after' => '</div>'
+	                )));
+	            endif;
+	        ?>
+	<?php } // end custom_comments
+	
+	// Custom callback to list pings
+	function custom_pings($comment, $args, $depth) {
+	       $GLOBALS['comment'] = $comment;
+	        ?>
+	            <li id="comment-<?php comment_ID() ?>" <?php comment_class() ?>>
+	                <div class="comment-author"><?php printf(__('By %1$s on %2$s at %3$s', 'tealseagull'),
+	                        get_comment_author_link(),
+	                        get_comment_date(),
+	                        get_comment_time() );
+	                        edit_comment_link(__('Edit', 'tealseagull'), ' <span class="meta-sep">|</span> <span class="edit-link">', '</span>'); ?></div>
+	    <?php if ($comment->comment_approved == '0') _e('\t\t\t\t\t<span class="unapproved">Your trackback is awaiting moderation.</span>\n', 'tealseagull') ?>
+	            <div class="comment-content">
+	                <?php comment_text() ?>
+	            </div>
+	<?php } // end custom_pings
+	
+	
+	// For category lists on category archives: Returns other categories except the current one (redundant)
+	function cats_meow($glue) {
+	    $current_cat = single_cat_title( '', false );
+	    $separator = "\n";
+	    $cats = explode( $separator, get_the_category_list($separator) );
+	    foreach ( $cats as $i => $str ) {
+	        if ( strstr( $str, ">$current_cat<" ) ) {
+	            unset($cats[$i]);
+	            break;
+	        }
+	    }
+	    if ( empty($cats) )
+	        return false;
+
+	    return trim(join( $glue, $cats ));
+	} // end cats_meow
+	
+	// For tag lists on tag archives: Returns other tags except the current one (redundant)
+	function tag_ur_it($glue) {
+	    $current_tag = single_tag_title( '', '',  false );
+	    $separator = "\n";
+	    $tags = explode( $separator, get_the_tag_list( "", "$separator", "" ) );
+	    foreach ( $tags as $i => $str ) {
+	        if ( strstr( $str, ">$current_tag<" ) ) {
+	            unset($tags[$i]);
+	            break;
+	        }
+	    }
+	    if ( empty($tags) )
+	        return false;
+
+	    return trim(join( $glue, $tags ));
+	} // end tag_ur_it
+	
+	// Register widgetized areas
+	function theme_widgets_init() {
+	    // Area 1
+	    register_sidebar( array (
+	    'name' => 'Primary Widget Area',
+	    'id' => 'primary_widget_area',
+	    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+	    'after_widget' => "</li>",
+	    'before_title' => '<h3 class="widget-title">',
+	    'after_title' => '</h3>',
+	  ) );
+
+	    // Area 2
+	    register_sidebar( array (
+	    'name' => 'Secondary Widget Area',
+	    'id' => 'secondary_widget_area',
+	    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+	    'after_widget' => "</li>",
+	    'before_title' => '<h3 class="widget-title">',
+	    'after_title' => '</h3>',
+	  ) );
+	} // end theme_widgets_init
+
+	add_action( 'init', 'theme_widgets_init' );
+	
+	$preset_widgets = array (
+	    'primary_widget_area'  => array( 'search', 'pages', 'categories', 'archives' ),
+	    'secondary_widget_area'  => array( 'links', 'meta' )
+	);
+	if ( isset( $_GET['activated'] ) ) {
+	    update_option( 'sidebars_widgets', $preset_widgets );
+	}
+	// update_option( 'sidebars_widgets', NULL );
+	
+	// Check for static widgets in widget-ready areas
+	function is_sidebar_active( $index ){
+	  global $wp_registered_sidebars;
+
+	  $widgetcolums = wp_get_sidebars_widgets();
+
+	  if ($widgetcolums[$index]) return true;
+
+	    return false;
 	} // end is_sidebar_active
